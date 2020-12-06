59bec7da0a1bff04c0bdcebc7936c17dbd384667
rychtaradam <rychtar.adamek@seznam.cz>
2020-12-06 12:04:03 +0100

    1. změna designu

--- PyGame/game.py
+++ PyGame/game.py
@@ -1,1 +1,46 @@
-import pyga
+import pygame
+
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surf = pygame.Surface((75, 25))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect()
+
+
+pygame.init()
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+player = Player()
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                running = False
+
+        elif event.type == QUIT:
+            running = False
+
+    screen.fill((0, 0, 0))
+
+    screen.blit(player.surf, player.rect)
+
+    pygame.display.flip()
