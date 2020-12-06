e837cf008f1b4702c8890145e7306d312b667033
WizzyKCZ <wizzy172@seznam.cz>
2020-12-06 12:42:43 +0100

    movement imput

--- PyGame/gameF.py
+++ PyGame/gameF.py
@@ -10,12 +10,41 @@ from pygame.locals import (
     QUIT,
 )
 
-pygame.init()
-
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surf = pygame.Surface((75, 25))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect()
+
+    def update(self, pressed_keys):
+        if pressed_keys[K_UP]:
+            self.rect.move_ip(0, -5)
+        if pressed_keys[K_DOWN]:
+            self.rect.move_ip(0, 5)
+        if pressed_keys[K_LEFT]:
+            self.rect.move_ip(-5, 0)
+        if pressed_keys[K_RIGHT]:
+            self.rect.move_ip(5, 0)
+
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > SCREEN_WIDTH:
+            self.rect.left = SCREEN_WIDTH
+        if self.rect.top <= 0:
+            self.rect.left = 0
+        if self.rect.bottom >= SCREEN_HEIGHT:
+            self.rect.bottom = SCREEN_HEIGHT
+
+pygame.init()
+
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+player = Player()
 
 running = True
 
@@ -28,17 +57,12 @@ while running:
         elif event.type == QUIT:
             running = False
 
-    screen.fill((255, 255, 255))
+    pressed_keys = pygame.key.get_pressed()
 
-    surf = pygame.Surface((50, 50))
+    player.update(pressed_keys)
 
-    surf.fill((0, 0, 0))
-    rect = surf.get_rect()
+    screen.fill((0, 0, 0))
 
-    surf_center = (
-        (SCREEN_WIDTH-surf.get_width())/2,
-        (SCREEN_HEIGHT - surf.get_height()) / 2
-    )
+    screen.blit(player.surf, player.rect)
 
-    screen.blit(surf, surf_center)
     pygame.display.flip()
